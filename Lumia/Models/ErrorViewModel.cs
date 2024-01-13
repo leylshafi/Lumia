765114ce9927d26e2d768e0e8e3c8ef0b0b81bc3
Leyla Shafiyeva <llshafi03@gmail.com>
2024-01-14 03:46:37 +0400

    Front to back

--- Lumia/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Lumia.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
